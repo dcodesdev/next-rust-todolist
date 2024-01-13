967717213a37f9820d3a9fa30e3e46ba9dc09c3a
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-13 14:19:58 +0300

    login route

--- packages/server/src/routes/user/mod.rs
+++ packages/server/src/routes/user/mod.rs
@@ -4,5 +4,7 @@ use axum::Router;
 pub mod handlers;
 
 pub fn user_routes() -> Router {
-    Router::new().route("/", post(handlers::register_user))
+    Router::new()
+        .route("/", post(handlers::register_user))
+        .route("/login", post(handlers::login_user))
 }
