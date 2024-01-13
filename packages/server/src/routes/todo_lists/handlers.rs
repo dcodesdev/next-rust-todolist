3eb5719e2ccfa4c499867af354117c54c57a27c7
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-13 19:14:00 +0300

    create list

--- packages/server/src/routes/todo_lists/handlers.rs
+++ packages/server/src/routes/todo_lists/handlers.rs
@@ -1,5 +1,5 @@
 use axum::{Extension, Json};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::{
@@ -40,4 +40,39 @@ pub async fn get_todo_lists(
     };
 
     Ok(Json(GetTodoListsResponse { data: lists }))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTodoListRequest {
+    name: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CreateTodoListResponse {
+    data: TodoList,
+}
+
+pub async fn create_todo_list(
+    Extension(user): Extension<ReqUser>,
+    Extension(state): Extension<AppState>,
+    Json(body): Json<CreateTodoListRequest>,
+) -> ApiResult<Json<CreateTodoListResponse>> {
+    let list = match sqlx::query_as!(
+        TodoList,
+        r#"
+        INSERT INTO lists (name, user_id)
+        VALUES ($1, $2)
+        RETURNING id, name, user_id, created_at, updated_at
+        "#,
+        body.name,
+        user.id
+    )
+    .fetch_one(&state.db)
+    .await
+    {
+        Ok(list) => list,
+        Err(_) => return Err(ApiError::InternalServerError),
+    };
+
+    Ok(Json(CreateTodoListResponse { data: list }))
 }
