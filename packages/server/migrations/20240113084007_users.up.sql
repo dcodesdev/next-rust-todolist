0a6356e3098b82bd4bbb714f3552c15cf01a11bf
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-13 11:42:38 +0300

    users table

---
+++ packages/server/migrations/20240113084007_users.up.sql
@@ -0,0 +1,11 @@
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
