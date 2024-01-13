6cdce195c8ffe167ca224f99ba9b354a77c1c7b6
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-13 14:00:56 +0300

    register user route

--- packages/server/src/routes/user/mod.rs
+++ packages/server/src/routes/user/mod.rs
@@ -1,10 +1,8 @@
+use axum::routing::post;
 use axum::Router;
 
 pub mod handlers;
 
 pub fn user_routes() -> Router {
-    Router::new()
-    // [TODO]: Implement user routes
-    // .route( "/", get().post())
-    // .route("/:id", post().put().delete())
+    Router::new().route("/", post(handlers::register_user))
 }
