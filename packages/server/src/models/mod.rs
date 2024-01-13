a6054c0dac3db53f72e8d64a3454d15cfe946465
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-13 19:11:07 +0300

    get todo lists

--- packages/server/src/models/mod.rs
+++ packages/server/src/models/mod.rs
@@ -1,1 +1,2 @@
+pub mod todo_list;
 pub mod user;
--- packages/server/src/routes/todo_lists/handlers.rs
+++ packages/server/src/routes/todo_lists/handlers.rs
@@ -1,16 +1,43 @@
-use axum::Json;
+use axum::{Extension, Json};
 use serde::Serialize;
 use uuid::Uuid;
 
-use crate::error::ApiResult;
+use crate::{
+    app::AppState,
+    error::{ApiError, ApiResult},
+    middlewares::ReqUser,
+    models::todo_list::TodoList,
+};
 
 #[derive(Debug, Serialize)]
 pub struct Todo {
     id: Uuid,
 }
 
-pub async fn get_todo_lists() -> ApiResult<Json<Todo>> {
-    let todo = Todo { id: Uuid::new_v4() };
+#[derive(Debug, Serialize)]
+pub struct GetTodoListsResponse {
+    data: Vec<TodoList>,
+}
 
-    Ok(Json(todo))
+pub async fn get_todo_lists(
+    Extension(user): Extension<ReqUser>,
+    Extension(state): Extension<AppState>,
+) -> ApiResult<Json<GetTodoListsResponse>> {
+    let lists = match sqlx::query_as!(
+        TodoList,
+        r#"
+        SELECT id, name, user_id, created_at, updated_at
+        FROM lists
+        WHERE user_id = $1
+        "#,
+        user.id
+    )
+    .fetch_all(&state.db)
+    .await
+    {
+        Ok(lists) => lists,
+        Err(_) => return Err(ApiError::InternalServerError),
+    };
+
+    Ok(Json(GetTodoListsResponse { data: lists }))
 }
