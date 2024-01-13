dbe5f8bddde7753e325e9d668fbc66ed623d92fb
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-13 19:14:00 +0300

    create list

--- packages/server/src/routes/todo_lists/mod.rs
+++ packages/server/src/routes/todo_lists/mod.rs
@@ -4,6 +4,9 @@ use axum::{middleware::from_fn, routing::get, Router};
 
 pub fn todo_lists_routes() -> Router {
     Router::new()
-        .route("/", get(handlers::get_todo_lists))
+        .route(
+            "/",
+            get(handlers::get_todo_lists).post(handlers::create_todo_list),
+        )
         .layer(from_fn(crate::middlewares::auth))
 }
