36aeaadf0bf72f1320162642d9ee63d871bfc976
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-13 21:34:31 +0300

    Get todos route

--- packages/server/src/routes/todos/handlers.rs
+++ packages/server/src/routes/todos/handlers.rs
@@ -1,23 +1,39 @@
-use axum::Json;
+use axum::{Extension, Json};
 use serde::Serialize;
 
-use crate::error::ApiResult;
+use crate::{
+    app::AppState,
+    error::{ApiError, ApiResult},
+    middlewares::ReqUser,
+    models::todo_item::TodoItem,
+};
 
 #[derive(Debug, Serialize)]
-pub struct TodoItem {
-    id: u64,
-    pub title: String,
-    pub completed: bool,
+pub struct GetTodosResponse {
+    pub data: Vec<TodoItem>,
 }
 
-pub async fn get_todos() -> ApiResult<Json<TodoItem>> {
-    // [TODO]: Implement get_todos
-
-    let todo = TodoItem {
-        id: 1,
-        title: "Hello".to_string(),
-        completed: false,
+pub async fn get_todos(
+    Extension(state): Extension<AppState>,
+    Extension(user): Extension<ReqUser>,
+) -> ApiResult<Json<GetTodosResponse>> {
+    let todos = match sqlx::query_as!(
+        TodoItem,
+        r#"
+        SELECT todo_items.id, title, description, completed, list_id,
+        todo_items.created_at, todo_items.updated_at
+        FROM todo_items
+        INNER JOIN lists ON lists.id = todo_items.list_id
+        WHERE lists.user_id = $1
+        "#,
+        &user.id
+    )
+    .fetch_all(&state.db)
+    .await
+    {
+        Ok(todos) => todos,
+        Err(_) => return Err(ApiError::InternalServerError),
     };
 
-    Ok(Json(todo))
+    Ok(Json(GetTodosResponse { data: todos }))
 }
