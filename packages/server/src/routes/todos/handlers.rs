7136027b5017d86df1c2ae4e9855cea1c41b2ab0
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-14 14:28:11 +0300

    Update todo

--- packages/server/src/routes/todos/handlers.rs
+++ packages/server/src/routes/todos/handlers.rs
@@ -1,4 +1,4 @@
-use axum::{Extension, Json};
+use axum::{extract::Path, Extension, Json};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -79,4 +79,49 @@ pub async fn create_todo(
     };
 
     Ok(Json(CreateTodoResponse { data: todo }))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateTodoRequest {
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub completed: Option<bool>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct UpdateTodoResponse {
+    pub data: TodoItem,
+}
+
+pub async fn update_todo(
+    Extension(state): Extension<AppState>,
+    Extension(user): Extension<ReqUser>,
+    Path(id): Path<Uuid>,
+    Json(body): Json<UpdateTodoRequest>,
+) -> ApiResult<Json<UpdateTodoResponse>> {
+    let todo = match sqlx::query_as!(
+        TodoItem,
+        r#"
+        UPDATE todo_items
+        SET title = COALESCE($1, title), description = COALESCE($2, description),
+        completed = COALESCE($3, completed)
+        FROM lists
+        WHERE todo_items.id = $4 AND todo_items.list_id = lists.id AND lists.user_id = $5
+        RETURNING todo_items.id, todo_items.title, todo_items.description, 
+        todo_items.completed, todo_items.list_id, todo_items.created_at, todo_items.updated_at
+        "#,
+        body.title,
+        body.description,
+        body.completed,
+        id,
+        user.id // Assuming user.id is the field with the user's ID
+    )
+    .fetch_one(&state.db)
+    .await
+    {
+        Ok(todo) => todo,
+        Err(_) => return Err(ApiError::InternalServerError),
+    };
+
+    Ok(Json(UpdateTodoResponse { data: todo }))
 }
--- packages/server/src/routes/todos/mod.rs
+++ packages/server/src/routes/todos/mod.rs
@@ -1,11 +1,16 @@
 pub mod handlers;
 
 use crate::middlewares;
-use axum::{middleware::from_fn, routing::get, Router};
+use axum::{
+    middleware::from_fn,
+    routing::{get, put},
+    Router,
+};
 
 // [TODO]: Implement todos routes
 pub fn todos_routes() -> Router {
     Router::new()
         .route("/", get(handlers::get_todos).post(handlers::create_todo))
+        .route("/:id", put(handlers::update_todo))
         .layer(from_fn(middlewares::auth))
 }
