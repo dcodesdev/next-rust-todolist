589be99fa9f2233a63d0aad32a56f60c00c7cfec
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-14 14:22:42 +0300

    create todo

--- packages/server/src/routes/todos/handlers.rs
+++ packages/server/src/routes/todos/handlers.rs
@@ -1,5 +1,6 @@
 use axum::{Extension, Json};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 use crate::{
     app::AppState,
@@ -36,4 +37,46 @@ pub async fn get_todos(
     };
 
     Ok(Json(GetTodosResponse { data: todos }))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTodoRequest {
+    pub title: String,
+    pub description: Option<String>,
+    pub list_id: Uuid,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CreateTodoResponse {
+    pub data: TodoItem,
+}
+
+pub async fn create_todo(
+    Extension(state): Extension<AppState>,
+    Extension(user): Extension<ReqUser>,
+    Json(body): Json<CreateTodoRequest>,
+) -> ApiResult<Json<CreateTodoResponse>> {
+    let todo = match sqlx::query_as!(
+        TodoItem,
+        r#"
+        INSERT INTO todo_items (title, description, list_id)
+        SELECT $1, $2, $3
+        FROM lists
+        WHERE lists.id = $3 AND lists.user_id = $4
+        RETURNING todo_items.id, todo_items.title, todo_items.description, 
+        todo_items.completed, todo_items.list_id, todo_items.created_at, todo_items.updated_at
+        "#,
+        &body.title,
+        body.description,
+        &body.list_id,
+        user.id // Assuming user.id is the field with the user's ID
+    )
+    .fetch_one(&state.db)
+    .await
+    {
+        Ok(todo) => todo,
+        Err(_) => return Err(ApiError::InternalServerError),
+    };
+
+    Ok(Json(CreateTodoResponse { data: todo }))
 }
--- packages/server/src/routes/todos/mod.rs
+++ packages/server/src/routes/todos/mod.rs
@@ -6,6 +6,6 @@ use axum::{middleware::from_fn, routing::get, Router};
 // [TODO]: Implement todos routes
 pub fn todos_routes() -> Router {
     Router::new()
-        .route("/", get(handlers::get_todos))
+        .route("/", get(handlers::get_todos).post(handlers::create_todo))
         .layer(from_fn(middlewares::auth))
 }
