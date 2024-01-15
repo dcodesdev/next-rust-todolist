7a53ba6b2bf81a3dfcccb80a0a6ff097d687fd24
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-15 23:04:49 +0300

    bindings generated by ts-rs

---
+++ packages/server/bindings/index.ts
@@ -0,0 +1,6 @@
+export * from "./LoginUserRequestBody";
+export * from "./RegisterUserRequestBody";
+export * from "./LoginUserResponse";
+export * from "./GetCurrentUserResponse";
+export * from "./RegisterUserResponse";
+export * from "./UserWithoutPassword";
