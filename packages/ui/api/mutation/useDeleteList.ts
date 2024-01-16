d7d6ab0e854580cd85748e4091b0f4b535f2328a
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 21:05:00 +0300

    removed mutation keys

--- packages/ui/api/mutation/useDeleteList.ts
+++ packages/ui/api/mutation/useDeleteList.ts
@@ -3,7 +3,6 @@ import { useMutation } from "@tanstack/react-query"
 
 export const useDeleteList = (id: string) => {
   return useMutation({
-    mutationKey: ["deleteList", id],
     mutationFn: () => del(`/lists/${id}`),
   })
 }
