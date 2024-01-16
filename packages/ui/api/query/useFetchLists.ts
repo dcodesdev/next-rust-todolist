71a6784583952ac87a6b34507591dd12e36ef4d8
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 11:51:53 +0300

    create todo list

--- packages/ui/api/query/useFetchLists.ts
+++ packages/ui/api/query/useFetchLists.ts
@@ -1,4 +1,4 @@
-import { client } from "@/utils"
+import { get } from "@/utils"
 import { useQuery } from "@tanstack/react-query"
 import { GetTodoListsResponse } from "server"
 
@@ -6,10 +6,8 @@ import { GetTodoListsResponse } from "server"
  * This hook gets all the todo lists that belong to the current user.
  */
 export const useFetchLists = () => {
-  return useQuery<GetTodoListsResponse>({
+  return useQuery({
     queryKey: ["lists"],
-    queryFn: async () => {
-      return await client.get("/lists")
-    },
+    queryFn: () => get<GetTodoListsResponse>("/lists"),
   })
 }
