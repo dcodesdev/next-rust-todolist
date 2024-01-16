936729c98cf6e24ed1d90f18c42f50c560dff83a
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-16 10:49:55 +0300

    dashboard states

--- packages/ui/app/dashboard/page.tsx
+++ packages/ui/app/dashboard/page.tsx
@@ -1,14 +1,45 @@
 "use client"
 
 import { Container } from "@/components/Container"
+import { useFetchLists } from "@/query/useFetchLists"
+
+const Loading = () => {
+  return <div>Loading</div>
+}
+
+const NoLists = () => {
+  return <div>No lists</div>
+}
+
+const Lists = () => {
+  return <div>Lists</div>
+}
+
+const conditions = {
+  loading: Loading,
+  noLists: NoLists,
+  lists: Lists,
+}
 
 export default function Dashboard() {
+  const { data, isLoading } = useFetchLists()
+
+  const condition = data?.data.length
+    ? "lists"
+    : isLoading
+      ? "loading"
+      : "noLists"
+
+  const Component = conditions[condition]
+
   return (
     <Container>
       <main>
         <h1 className="text-4xl font-bold text-center">Dashboard Page</h1>
 
-        <div className="mt-20"></div>
+        <div className="mt-20">
+          <Component />
+        </div>
       </main>
     </Container>
   )
