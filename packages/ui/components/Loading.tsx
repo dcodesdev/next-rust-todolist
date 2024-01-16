57085da30181f8586df3063bb63df44321593956
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 20:42:14 +0300

    use fetch list loading todo item page

---
+++ packages/ui/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Container } from "./Container"
+
+export const Loading = () => {
+  return (
+    <Container className="min-h-screen flex justify-center items-center">
+      Loading...
+    </Container>
+  )
+}
