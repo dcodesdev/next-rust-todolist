1c738f16bbb7cfb5e2287fdf7df5fb4227cd4a74
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 12:34:07 +0300

    list item

--- packages/ui/components/pages/dashboard/ListItem.tsx
+++ packages/ui/components/pages/dashboard/ListItem.tsx
@@ -1,5 +1,6 @@
 import { Card } from "@/components/ui/card"
 import { dayjs } from "@/utils/dayjs"
+import { Trash } from "lucide-react"
 import { FC } from "react"
 import { GetTodoListsResponse } from "server"
 
@@ -8,8 +9,19 @@ export const ListItem: FC<{
 }> = ({ list }) => {
   return (
     <Card className="flex items-center justify-between p-2 gap-2">
-      <div>{list.name}</div>
-      <div>{dayjs(list.created_at).fromNow()}</div>
+      <div>
+        <p>{list.name}</p>
+
+        <small className="text-gray-400">
+          {dayjs(list.created_at).fromNow()}
+        </small>
+      </div>
+
+      <div className="flex items-center gap-1">
+        <div>
+          <Trash size={20} />
+        </div>
+      </div>
     </Card>
   )
 }
