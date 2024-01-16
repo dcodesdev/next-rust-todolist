5e1ea7567fbf792ccdc599652e945200f77080e2
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 10:43:29 +0300

    Dashboard use fetch lists

---
+++ packages/ui/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Container } from "@/components/Container"
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <main>
+        <h1 className="text-4xl font-bold text-center">Dashboard Page</h1>
+
+        <div className="mt-20"></div>
+      </main>
+    </Container>
+  )
+}
