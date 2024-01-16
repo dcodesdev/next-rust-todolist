6419c4dee87c16fe43e58a2b0e206571c2a28043
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 11:40:35 +0300

    toast error update

--- packages/ui/utils/toast.ts
+++ packages/ui/utils/toast.ts
@@ -2,15 +2,12 @@ import { AxiosError } from "axios"
 import { toast } from "sonner"
 
 export const toastError = (e: unknown | string) => {
-  if (typeof e === "string") {
-    toast.error(e)
-    return
-  }
-
   if (e instanceof AxiosError) {
     toast.error(e.response?.data?.message || e.message)
   } else if (e instanceof Error) {
     toast.error(e.message)
+  } else if (typeof e === "string") {
+    toast.error(e)
   } else {
     toast.error("An error occurred")
   }
