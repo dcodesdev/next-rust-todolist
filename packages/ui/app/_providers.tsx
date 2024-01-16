a66d89a8e2e448c556624ebdfd8ddf19945386d3
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2024-01-16 10:33:12 +0300

    react query

--- packages/ui/app/_providers.tsx
+++ packages/ui/app/_providers.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { FC, ReactNode } from "react"
-import { QueryClient, QueryClientProvider } from "react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
---
+++ packages/ui/app/login/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+import { Container } from "@/components/Container"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { client } from "@/utils"
+import { FormEvent, useRef } from "react"
+import { LoginUserResponse } from "server"
+import Cookies from "js-cookie"
+import { useMutation } from "@tanstack/react-query"
+import { toastError } from "@/utils/toast"
+
+export default function Login() {
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  const { mutateAsync: onSubmit, isPending } = useMutation({
+    mutationFn: async (e: FormEvent) => {
+      e.preventDefault()
+
+      const email = emailRef.current?.value
+      const password = passwordRef.current?.value
+
+      if (!email) return
+      if (!password) return
+
+      return client
+        .post<LoginUserResponse>("/user/login", { email, password })
+        .then((r) => {
+          Cookies.set("token", r.data.token)
+          window.location.href = "/dashboard"
+        })
+        .catch(toastError)
+    },
+  })
+
+  return (
+    <Container>
+      <h1 className="text-4xl font-bold text-center">Login</h1>
+
+      <Card className="mt-28 max-w-sm mx-auto">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+
+        <form onSubmit={onSubmit}>
+          <CardContent>
+            <Label>Email</Label>
+
+            <Input
+              ref={emailRef}
+              type="email"
+              placeholder="Enter your email address"
+            />
+
+            <Label className="mt-3">Password</Label>
+
+            <Input
+              ref={passwordRef}
+              type="password"
+              placeholder="Enter your password"
+            />
+          </CardContent>
+
+          <CardFooter>
+            <Button disabled={isPending}>Login</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </Container>
+  )
+}
