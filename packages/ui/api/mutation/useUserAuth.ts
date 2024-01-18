e1f481dab73d3ab4f4730e77c34c3860d4d789f4
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-01-18 12:37:15 +0300

    login page fix

--- packages/ui/api/mutation/useUserAuth.ts
+++ packages/ui/api/mutation/useUserAuth.ts
@@ -1,9 +1,8 @@
-import { post } from "@/utils"
-import { useMutation } from "@tanstack/react-query"
-import { LoginUserResponse, RegisterUserResponse } from "server"
-import Cookies from "js-cookie"
-import { useRouter } from "next/navigation"
-import { z } from "zod"
+import { post } from "@/utils";
+import { useMutation } from "@tanstack/react-query";
+import { LoginUserResponse, RegisterUserResponse } from "server";
+import Cookies from "js-cookie";
+import { z } from "zod";
 
 const userLoginSchema = z.object({
   email: z
@@ -17,35 +16,33 @@ const userLoginSchema = z.object({
     })
     .min(1, "Password is required"),
   name: z.string().optional(),
-})
+});
 
 export const useUserAuth = (isRegister = false) => {
-  const router = useRouter()
-
   return useMutation({
     mutationFn: async (body: any) => {
-      const parsed = userLoginSchema.safeParse(body)
+      const parsed = userLoginSchema.safeParse(body);
 
       if (!parsed.success) {
-        throw new Error(parsed.error.issues[0].message)
+        throw new Error(parsed.error.issues[0].message);
       }
 
-      const { email, password } = parsed.data
+      const { email, password } = parsed.data;
 
       return isRegister
         ? post<RegisterUserResponse>("/user", {
             email,
             password,
           }).then((r) => {
-            Cookies.set("token", r.token)
-            router.push("/dashboard")
+            Cookies.set("token", r.token);
+            window.location.href = "/dashboard";
           })
         : post<LoginUserResponse>("/user/login", { email, password }).then(
             (r) => {
-              Cookies.set("token", r.token)
-              router.push("/dashboard")
-            }
-          )
+              Cookies.set("token", r.token);
+              window.location.href = "/dashboard";
+            },
+          );
     },
-  })
-}
+  });
+};
